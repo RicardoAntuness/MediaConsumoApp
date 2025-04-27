bfd4ed78df4ac87ff1b4bb29282c9a83a2de9945
RicardoAntuness <ricardo_rantunes22@hotmail.com>
2025-04-27 16:23:53 -0300

    Primeiro commit projeto

---
+++ App.js
@@ -0,0 +1,17 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+import ResultScreen from './screens/ResultScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Resultado" component={ResultScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
